61630d6c2c0e5ce4df81f5dfc8575355b03be8f6
Alan-Uni <144484127+Alan-Uni@users.noreply.github.com>
2023-10-07 13:44:55 -0600

    Valoracion terminada

--- Practica-04-FormulariotxtPython/Practica_04_FormulariotxtPython.py
+++ Practica-04-FormulariotxtPython/Practica_04_FormulariotxtPython.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk
 from tkinter import messagebox
+import re
 def limpiar_campos():
     entry_nombre.delete(0, tk.END)
     entry_apellidos.delete(0, tk.END)
@@ -9,8 +10,6 @@ def limpiar_campos():
     entry_telefono.delete(0, tk.END)
     var_genero.set(0)
 
-def borrar_campos():
-    limpiar_campos()
 
 def guardar_datos():
     nombre = entry_nombre.get()
@@ -23,24 +22,37 @@ def guardar_datos():
        genero="Hombre"
     elif var_genero.get()==2:
        genero="Mujer"
+    if(es_entero_valido(edad) and es_decimal_valido(estatura) and es_entero_valido_de_10_digitos(telefono) and es_texto_valido(nombre) and es_texto_valido(apellidos)):
 
-    datos= f"Nombres: {nombre}\nApellidos: {apellidos}\nEdad:  {edad} años\nEstatura: {estatura}cm\nTelefono:  {telefono}\nGenero: {genero}"
-
-    with open("datos99.txt","a") as archivo:
-       archivo.write(datos+ "\n\n")
+        datos= f"Nombres: {nombre}\nApellidos: {apellidos}\nEdad:  {edad} años\nEstatura: {estatura}cm\nTelefono:  {telefono}\nGenero: {genero}"
+        with open("datos99.txt","a") as archivo:
+            archivo.write(datos+ "\n\n")
 
 
     
-    messagebox.showinfo("Informacion","Datos guardados con exito\n\n" + datos)
+        messagebox.showinfo("Informacion","Datos guardados con exito\n\n" + datos)
 
-    entry_nombre.delete(0, tk.END)
-    entry_apellidos.delete(0, tk.END)
-    entry_edad.delete(0, tk.END)
-    entry_estatura.delete(0, tk.END)
-    entry_telefono.delete(0, tk.END)
-    var_genero.set(0)
+        limpiar_campos()
+    else:
+        messagebox.showerror("error", "porfavor,ingreso datos validos en los campos")
 
+def es_entero_valido(valor):
+    try:
+        int(valor)
+        return True
+    except ValueError:
+        return False
 
+def es_decimal_valido(valor):
+    try:
+        float(valor)
+        return True
+    except ValueError:
+        return False
+def es_entero_valido_de_100_digitos(valor):
+    return valor.isdigit() and len(valor) == 10
+def es_texto_valido(valor):
+    return bool(re.match("^[a-zA-Z\s]+$",valor))
 
 formulario = tk.Tk()
 formulario.title("Formulario")
@@ -78,13 +90,13 @@ label_genero.pack()
 rb_hombre = tk.Radiobutton(formulario, text="Hombre", variable=var_genero, value=1)
 rb_hombre.pack()
 
-rb_hombre = tk.Radiobutton(formulario, text="Mujer", variable=var_genero, value=2)
-rb_hombre.pack()
+rb_mujer = tk.Radiobutton(formulario, text="Mujer", variable=var_genero, value=2)
+rb_mujer.pack()
 
 btn_guardar = tk.Button(formulario, text= "Guardar", command=guardar_datos)
 btn_guardar.pack()
 
-btn_borrar = tk.Button(formulario, text= "Borrar campos", command=borrar_campos)
+btn_borrar = tk.Button(formulario, text= "Borrar campos", command=limpiar_campos)
 btn_borrar.pack()
 
 formulario.mainloop()
